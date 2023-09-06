999cf35ebfc09d4cae05f76003163ec870797890
ukssss <1872003115su@gmail.com>
2023-09-06 13:27:18 +0900

    Feat | SearchPage 제작

---
+++ src/pages/SearchPage.tsx
@@ -0,0 +1,36 @@
+import { useState } from 'react';
+import { styled } from 'styled-components';
+
+import { SearchBar, SearchResult, Title } from '@/components';
+import { useLocalCache } from '@/hooks';
+
+const SearchPage = () => {
+  const [diseaseName, setDiseaseName] = useState('');
+
+  const changeDiseaseName = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setDiseaseName(e.target.value);
+  };
+
+  const diseaseList = useLocalCache(diseaseName);
+
+  return (
+    <StyledContainer>
+      <Title />
+      <SearchBar
+        id='clinical-trial-search-input'
+        placeholder='질환명을 입력해 주세요'
+        value={diseaseName}
+        onChange={changeDiseaseName}
+      />
+      <SearchResult diseaseList={diseaseList} diseaseName={diseaseName} />
+    </StyledContainer>
+  );
+};
+
+const StyledContainer = styled.div`
+  height: 100vh;
+  padding: 50px;
+  background-color: #cae9ff;
+`;
+
+export default SearchPage;
